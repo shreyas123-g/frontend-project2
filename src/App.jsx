2fa792a969f3153bf777e3cb614f29b2ca252795
Gamana <gamana.rs@gmail.com>
2025-10-14 10:40:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserInfo from './UserInfo'
+import Button from './Button'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, Redux!</h1>
+      <UserInfo />
+      <Button />
+    </div>
+  )
+}
